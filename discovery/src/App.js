968196640d1638d5c6cb51e7c0aeda86d3d3bab5
Kristian Hajredinaj <hajredinajk@gmail.com>
2023-09-08 17:50:36 -0400

    showing valid file message

--- discovery/src/App.js
+++ discovery/src/App.js
@@ -1,18 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Leafletmap from './components/Leafletmap';
 
+
 function App() {
+  const [mapData, setMapData] = useState(null);
+  const [validFileMessage, setValidFileMessage] = useState("Waiting for file")
+  const [selectedFile, setSelectedFile] = useState(null)
+  console.log(validFileMessage)
+
+  const correctTypes = ['application/kml', 'application/zip', 'application/json']
+
+  const handleFileChange = async (event) => { // Handle file input, here is where to add other file types
+     setSelectedFile(event.target.files[0]);
+
+    if (selectedFile) {
+      try {
+        const fileContent = await selectedFile.text();
+        // Parse JSON file
+        const parsedData = JSON.parse(fileContent);
+        setMapData(parsedData);
+
+ 
+      } catch (error) {
+        console.log('Error with file', error);
+      }
+    }
+  };
+
+  //refreshes the screen to update the correct file message
+  useEffect(() => {
+    if(correctTypes.includes(selectedFile?.type)){
+        setValidFileMessage("It is a valid file")
+    } else { 
+        setValidFileMessage("It is NOT a valid file")
+    }      
+}, [handleFileChange])
+
+
   return (
     <div className="App">
       <h1>Discovery Part 1</h1>
       <label for="mapfile">Choose a map file:</label><br></br>
-      <input type="file" id="mapfile" name="mapfile" accept=""/>
+      <input type="file" id="mapfile" name="mapfile" accept="" onSubmit={handleFileChange}/>
       <input type="button" value="Generate"></input>
-      <h4>Is valid file</h4>
+      <h4>{validFileMessage}</h4>
       <div id="map">
-      <Leafletmap/>
+      <Leafletmap file={mapData}/>
       </div>
       </div>
   );
