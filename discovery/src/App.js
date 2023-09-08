fa8a262e31a5340b60ab959c3f329055f0e55cbc
Zachary Loiwnger <zackxbl4@gmail.com>
2023-09-08 17:21:53 -0400

    Map Accepting GeoJson Files From User

--- discovery/src/App.js
+++ discovery/src/App.js
@@ -1,18 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Leafletmap from './components/Leafletmap';
 
 function App() {
+  const [mapData, setMapData] = useState(null);
+
+  const handleFileChange = async (event) => { // Handle file input, here is where to add other file types
+    const selectedFile = event.target.files[0];
+
+    if (selectedFile) {
+      try {
+        const fileContent = await selectedFile.text();
+        // Parse JSON file
+        const parsedData = JSON.parse(fileContent);
+        setMapData(parsedData);
+      } catch (error) {
+        console.log('Error with file', error);
+      }
+    }
+  };
+
+
   return (
     <div className="App">
       <h1>Discovery Part 1</h1>
       <label for="mapfile">Choose a map file:</label><br></br>
-      <input type="file" id="mapfile" name="mapfile" accept=""/>
+      <input type="file" id="mapfile" name="mapfile" accept="" onChange={handleFileChange}/>
       <input type="button" value="Generate"></input>
       <h4>Is valid file</h4>
       <div id="map">
-      <Leafletmap/>
+      <Leafletmap file={mapData}/>
       </div>
       </div>
   );
