b77a0c916081f330c08ed6f71fc5ad59a4eea74d
Kristian Hajredinaj <hajredinajk@gmail.com>
2023-09-08 18:23:35 -0400

    Valid File message working

--- discovery/src/App.js
+++ discovery/src/App.js
@@ -1,41 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Leafletmap from './components/Leafletmap';
-
 
 function App() {
   const [mapData, setMapData] = useState(null);
   const [validFileMessage, setValidFileMessage] = useState("Waiting for file")
-  const [selectedFile, setSelectedFile] = useState(null)
-  console.log(validFileMessage)
-
   const correctTypes = ['application/kml', 'application/zip', 'application/json']
 
   const handleFileChange = async (event) => { // Handle file input, here is where to add other file types
-     setSelectedFile(event.target.files[0]);
+    const selectedFile = event.target.files[0];
 
     if (selectedFile) {
       try {
+        //check if the file type is valid
+        if(correctTypes.includes(selectedFile?.type)){
+          setValidFileMessage("It is a valid file")
+        } else { 
+          setValidFileMessage("It is NOT a valid file")
+        }     
         const fileContent = await selectedFile.text();
         // Parse JSON file
         const parsedData = JSON.parse(fileContent);
         setMapData(parsedData);
-
       } catch (error) {
-        console.log('Error with file', error);
+        setValidFileMessage('Error with file', error);
       }
     }
   };
-
-  //refreshes the screen to update the correct file message
-  useEffect(() => {
-    if(correctTypes.includes(selectedFile?.type)){
-        setValidFileMessage("It is a valid file")
-    } else { 
-        setValidFileMessage("It is NOT a valid file")
-    }      
-}, [handleFileChange])
 
 
   return (
