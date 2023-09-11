65a9f313869b5b17977934aeaa528c58a044acd1
Cameron <cameron.grieco@stonybrook.edu>
2023-09-11 15:55:29 -0400

    shapefile to geojson not working

--- discovery/src/App.js
+++ discovery/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { useState } from 'react';
 import Leafletmap from './components/Leafletmap';
+import  shp  from 'shpjs';
 
 function App() {
   const [mapData, setMapData] = useState(null);
@@ -22,6 +23,68 @@ function App() {
           setValidFileMessage("It is NOT a valid file")
         }     
         const fileContent = await selectedFile.text();
+        console.log('selected file:', selectedFile);
+
+        const fileType = selectedFile.name.substr(selectedFile.name.length-4, selectedFile.name.length);
+        if(fileType === '.zip'){
+          console.log('Is a zip');
+    
+          	//for the shapefiles in the folder called 'files' with the name pandr.shp
+            
+            // proof the package is installed correctly
+            // await shp("C:/Users/camgr/Documents/Downloads/DEU_adm.zip").then(function(geojson){
+            //});
+
+            // var buffer = new ArrayBuffer(selectedFile.size); // not needed
+            // const buffer = await selectedFile.arrayBuffer();
+
+            await selectedFile.arrayBuffer().then(async function(buffer){
+              console.log("arrayBuffer:", buffer);
+
+              // SHP+buffer Direct
+              /*
+              const geojson = await shp(buffer);
+              console.log("passed check");
+              console.log('geojson', geojson);
+              */
+              
+              // SHP+buffer then
+              //await shp(buffer).then(function(geojson) {
+              //  console.log('passed Test');
+              //  console.log('geojson', geojson);
+              //});     
+              
+              // SHP+file Direct
+              /*
+              const geojson = await shp(selectedFile);
+              console.log("passed check");
+              console.log('geojson', geojson);
+              */
+              
+              // SHP+file then
+              /*
+              await shp(selectedFile).then(function(geojson) {
+                console.log('passed Test');
+                console.log('geojson', geojson);
+              });  
+              */
+
+             });
+            
+            
+
+            //await shp.getShapeFile(buffer).then(function(geojson){
+              //see bellow for whats here this internally call shp.parseZip()
+            // console.log('Passed check');
+            // console.log('geojson:', geojson);
+            //});
+            console.log('Passed check');
+            
+            
+        }
+        console.log('passedCheck');
+
+
         // Parse JSON file
         const parsedData = JSON.parse(fileContent);
         setMapData(parsedData);
@@ -35,9 +98,9 @@ function App() {
   return (
     <div className="App">
       <h1>Discovery Part 1</h1>
-      <label for="mapfile">Choose a map file:</label><br></br>
+      <label htmlFor="mapfile">Choose a map file:</label><br></br>
       <input type="file" id="mapfile" name="mapfile" accept="" onChange={handleFileChange}/>
-      <input type="button" value="Generate"></input>
+      <input type="button" value="Generate"></input> 
       <h4>{validFileMessage}</h4>
       <div id="map">
       <Leafletmap file={mapData}/>
